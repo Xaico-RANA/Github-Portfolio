df17c705d71b3f0fd3dcb02a375cefbdac8ee1a7
Mohammad RANA <ruhulaminrana98@gmail.com>
2025-01-26 15:29:57 +0600

    Update user-data.js

--- data/user-data.js
+++ data/user-data.js
@@ -2,12 +2,12 @@ export const userData = {
   githubUser: 'Xaico-RANA',
   devUsername: "Xaico-RANA",
   github: 'https://github.com/Xaico-RANA',
-  facebook: 'https://www.facebook.com/XAICO-RANS/',
+  facebook: 'https://www.facebook.com/XAICO-RANA/',
   linkedIn: 'https://bd.linkedin.com/in/mohammad-rana-b007b8325',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  twitter: 'https://twitter.com/',
+  stackOverflow: 'https://stackoverflow.com/users/29369606/mohammad-rana/',
+  leetcode: "https://leetcode.com/u/xaico-rana/",
+  resume: "https://drive.google.eyd/1eyutpKFFh",
   skills: ['React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
   timezone: '+6'
 };
